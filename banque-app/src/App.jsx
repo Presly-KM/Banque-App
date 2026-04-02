f864d3f68ded15da07edb8b18a711e4cef793398
Presly K.MALONGO <preslykomba.m@gmail.com>
2026-04-02 13:21:12 +0200

    import et utilisation de la function CarteClient()

--- banque-app/src/App.jsx
+++ banque-app/src/App.jsx
@@ -1,12 +1,19 @@
+import CarteClient from './CarteClient'                // On dit à App d'aller chercher le fichier voisin
+
 function App() {
   return (
-    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+    <div style={{ padding: '40px', fontFamily: 'Arial, sans-serif' }}>
       <h1 style={{ color: '#2c3e50' }}>🏦 Ma Banque Digitale</h1>
-      <p>Bienvenue dans l'interface de gestion, Presly.</p>
+      <p>Gestionnaire de comptes : <strong>Presly</strong></p>
       <hr />
-      <p>Le système est prêt pour l'intégration des données clients.</p>
+
+      {/* On crée 3 instances de notre composant avec des données différentes */}
+      <CarteClient nom="Presly (Moi)" solde={8500} />
+      <CarteClient nom="Jean Dupont" solde={-120} />
+      <CarteClient nom="Marie Curie" solde={12450} />
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
