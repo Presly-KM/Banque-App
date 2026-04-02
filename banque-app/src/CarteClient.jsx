53ac4a7e45bfdf36d131710f8ac671eeeac542b9
Presly K.MALONGO <preslykomba.m@gmail.com>
2026-04-02 13:44:04 +0200

    CarteCLient Ajout condition Débiteur

--- banque-app/src/CarteClient.jsx
+++ banque-app/src/CarteClient.jsx
@@ -1,14 +1,23 @@
 function CarteClient({ nom, solde }) {
+  // On crée une variable pour savoir si le client est à découvert
+  const estDebiteur = solde < 0;
+
   return (
     <div style={{ 
       border: '1px solid #ccc', 
       padding: '20px', 
       borderRadius: '10px',
       margin: '10px 0',
-      backgroundColor: '#f4f7f6'
+      // On change le fond en rouge très clair si le client est débiteur
+      backgroundColor: estDebiteur ? '#fff5f5' : '#f4f7f6' 
     }}>
       <h3>Client : {nom}</h3>
       <p>Solde actuel : <strong>{solde} €</strong></p>
+
+      {/* C'est ICI qu'on ajoute la mention manquante */}
+      <p style={{ color: estDebiteur ? 'red' : 'green', fontWeight: 'bold' }}>
+        Statut : {estDebiteur ? "⚠️ Débiteur (Alerte)" : "✅ Créditeur"}
+      </p>
     </div>
   );
 }
